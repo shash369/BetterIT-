d2c3274d3465fa21b265bb6bfa716ee87f6f3c88
Shashwat <shuklashashwat009@gmail.com>
2026-09-13 15:38:28 +0530

    initial setup done

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,4 +2,11 @@ import { Text, View } from "react-native";
 import '../../global.css'
 
 export default function Index() {
-
+  return (
+    <View>
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to <Text className="text-red-300">BetterIT</Text> 
+      </Text>
+    </View>
+  );
+}
